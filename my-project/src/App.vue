fa85dbdaba9b3a8f7be7fb5bfeae6887e4089498
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-2.local>
2022-03-22 12:42:34 +0100

    martes

---
+++ my-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld />
+  <Counter />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    Counter,
+  },
+};
+</script>
+
+<style></style>
---
+++ my-project/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>The count is: {{ count }}</div>
+  <button class="boder px-4 py-2 rounder" @click="count++">+1</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cont: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
