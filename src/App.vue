3ecc2f973c91c46af79e0b3696c6e2f90423a0ff
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-2.local>
2022-03-24 11:31:33 +0100

    arreglar node modules

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <HelloWorld />
-  <Counter />
+  <div class="container mx-auto p-4">
+    <div>Hola</div>
+    <!-- 3. utilitzar el component al template -->
+    <input type="number" v-model.number="max1" class="border" />
+    <Counter :max="max1" />
+    <Counter :max="10" />
+    <Counter :max="20" />
+  </div>
 </template>
-
 <script>
+// 1: importar el component
 import HelloWorld from "./components/HelloWorld.vue";
 import Counter from "./components/Counter.vue";
-
 export default {
+  // 2: registrar el component
   components: {
     HelloWorld,
     Counter,
+  },
+  data() {
+    return {
+      max1: 10,
+    };
   },
 };
 </script>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>The count is: {{ count }}</div>
-  <button class="boder px-4 py-2 rounder" @click="count++">+1</button>
+  <div class="bg-gray-100 mb-4 p-2 rounded shadow">
+    <div>
+      The count is:
+      <span
+        :class="{
+          'text-red-600': count === max,
+          'text-orange-400': count >= max - 3 && count < max,
+          'text-green-600': count < max - 3,
+        }"
+        >{{ count }}</span
+      >
+    </div>
+    <button class="border px-4 py-2 rounded bg-white" @click="decrease">
+      -1
+    </button>
+    <button
+      class="border px-4 py-2 rounded bg-white"
+      :class="{ 'text-gray-300': count === max }"
+      :disabled="count === max"
+      @click="increase"
+    >
+      +1
+    </button>
+  </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      cont: 0,
-    };
+  props: ["max"],
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    decrease() {
+      if (this.count > 0) this.count--;
+    },
+    increase() {
+      if (this.count < this.max) this.count++;
+    },
   },
 };
 </script>
-
-<style></style>
