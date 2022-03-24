aaa663cf8c30de2f6aae7d4506772e3c3b4aac8e
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-2.local>
2022-03-24 13:21:47 +0100

    vueexemp

--- src/components/Bank.vue
+++ src/components/Bank.vue
@@ -1,25 +1,28 @@
 <template>
   <h1>Bank</h1>
-  <div>Income:{{ income }}€</div>
-  <div>Income:{{ outcome }}€</div>
-  <div>Income:{{ balance }}€</div>
+  <div>Income: {{ income }}€</div>
+  <div>Outcome: {{ outcome }}€</div>
+  <div>Balance: {{ balance }}€</div>
 
-  <imput type="text" class="border" v-model="name" placeholder="Name" />
-  <imput type="number" class="border" v-model="amount" placeholder="Amount" />
-  <button class="bg-teal-500 text-white px-2 rounded" key="" @clik="addItem">
+  <input type="text" class="border" v-model="name" placeholder="Name" />
+
+  <input type="number" class="border" v-model="amount" placeholder="Amount" />
+
+  <button class="bg-teal-500 text-white px-2 rounded" @click="addItem">
     Add
   </button>
+
   <ul>
     <li v-for="(item, i) in items" :key="i">
       {{ item.name }} {{ item.amount }}€
     </li>
   </ul>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      outcome: -30,
       name: "",
       amount: 0,
       items: [],
@@ -28,11 +31,16 @@ export default {
   computed: {
     income() {
       return this.items
-        .filter((item) => acc + item.amount > 0)
-        .reduce((acc, item) => acc + item < 0);
+        .filter((item) => item.amount > 0)
+        .reduce((acc, item) => acc + item.amount, 0);
+    },
+    outcome() {
+      return this.items
+        .filter((item) => item.amount < 0)
+        .reduce((acc, item) => acc + item.amount, 0);
     },
     balance() {
-      return this.icome + this.outcome;
+      return this.income + this.outcome;
     },
   },
   methods: {
@@ -44,6 +52,4 @@ export default {
     },
   },
 };
-
-// Laura i Javi updating
 </script>
